482ffbfb8270d5d5d5411f17afc9ae40feba4fcc
franck-tchassi <franck.tchassi@laplateforme.io>
2025-01-19 17:53:36 +0100

    ajout de la logique du fichier Apropos

--- app/(landing)/_sections/Apropos.tsx
+++ app/(landing)/_sections/Apropos.tsx
@@ -32,12 +32,12 @@ const About = () => {
             À propos de SMS Services
           </h2>
           <p className="text-gray-600  leading-relaxed mb-6 text-base">
-            Chez <span className="font-semibold text-blue-600">SMS SERVICES</span>, nous sommes fiers d'offrir des services de nettoyage automobile de qualité supérieure. 
-            Notre équipe passionnée s'engage à redonner à votre véhicule son éclat d'origine, en utilisant des techniques modernes et des produits respectueux de l'environnement.
+            Chez <span className="font-semibold text-blue-600">SMS SERVICES</span>, nous sommes fiers d&apos;offrir des services de nettoyage automobile de qualité supérieure. 
+            Notre équipe passionnée s&apos;engage à redonner à votre véhicule son éclat d&apos;origine, en utilisant des techniques modernes et des produits respectueux de l&apos;environnement.
           </p>
           <p className="text-gray-600 text-base leading-relaxed mb-6">
             Basés à <span className="font-semibold text-blue-600">Le Thor</span>, nous vous accueillons dans un cadre convivial et professionnel. 
-            Faites confiance à notre expertise pour prendre soin de votre voiture, à l'intérieur comme à l'extérieur.
+            Faites confiance à notre expertise pour prendre soin de votre voiture, à l&apos;intérieur comme à l&apos;extérieur.
           </p>
           <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-lg transition-all">
              <Link href={"/apropos"}>En savoir plus</Link> 
