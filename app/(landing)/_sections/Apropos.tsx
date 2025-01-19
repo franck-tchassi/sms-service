a3b21f3ae336f9e780710cf2a63f6e15d0ea3ddd
franck-tchassi <franck.tchassi@laplateforme.io>
2025-01-19 18:38:58 +0100

    animation enlevée

--- app/(landing)/_sections/Apropos.tsx
+++ app/(landing)/_sections/Apropos.tsx
@@ -3,9 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+
 
 const About = () => {
  
